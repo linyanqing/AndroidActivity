ad355b110af51e9119bc00cd60e871e2e0661a83
Yanqing Lin <yanqing.lin@news.com.au>
2015-05-19 10:43:13 +1000

    pass the variable via intent

--- app/src/main/java/au/com/pactera/androidactivity/MainActivity.java
+++ app/src/main/java/au/com/pactera/androidactivity/MainActivity.java
@@ -1,9 +1,11 @@
 package au.com.pactera.androidactivity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +14,54 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("onCreate");
+
+        findViewById(R.id.btnStartActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AnotherActivity.class);
+                i.putExtra("data", "Hello Pactera");
+                startActivity(i);
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        System.out.println("onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStart();
+        System.out.println("onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        System.out.println("onDestroy");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        System.out.println("onRestart");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
